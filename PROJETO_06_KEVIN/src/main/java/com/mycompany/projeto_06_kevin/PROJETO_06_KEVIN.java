90ad9d6db6ebc4bf6c1b9bb9d440d46f78f2e9d3
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-09-26 15:41:44 -0300

    teste

---
+++ PROJETO_06_KEVIN/src/main/java/com/mycompany/projeto_06_kevin/PROJETO_06_KEVIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_06_kevin;
+
+/**
+ *
+ * @author kevin
+ */
+public class PROJETO_06_KEVIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
